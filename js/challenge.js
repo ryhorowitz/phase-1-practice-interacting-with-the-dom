4dc7a168fd4e38dab8ffc22ec4c53b26df2d2d96
ryhorowitz <ryan.w.horowitz@gmail.com>
2022-12-27 17:04:23 -0500

    reset button functional

--- js/challenge.js
+++ js/challenge.js
@@ -8,7 +8,7 @@ const pauseBtn = document.querySelector('#pause')
 const likesList = document.querySelector('.likes')
 const likeObj = {}
 let number = Number(counter.textContent)
-let paused = false;
+let isPaused = false;
 
 
 
@@ -21,22 +21,26 @@ document.addEventListener('DOMContentLoaded', () => {
   let interval = setInterval(incrementCounter, 1000)
 
   // Manually increment and decrement the counter using the plus and minus buttons.
-  minusBtn.addEventListener('click', () => { if (!paused) { 
-    number--
-    counter.textContent = number.toString()
-  }})
-  plusBtn.addEventListener('click', () => { if (!paused) {
-    number++
-    counter.textContent = number.toString()
-  }})
+  minusBtn.addEventListener('click', () => {
+    if (!isPaused) {
+      number--
+      counter.textContent = number.toString()
+    }
+  })
+  plusBtn.addEventListener('click', () => {
+    if (!isPaused) {
+      number++
+      counter.textContent = number.toString()
+    }
+  })
 
   // Pause the counter, which should:
   // pause the counter
   // disable all buttons except the pause button
   // switch the label on the button from "pause" to "resume"
-  pauseBtn.addEventListener('click', () => {
-    paused = !paused
-    if (!paused) {
+  function pause() {
+    isPaused = !isPaused
+    if (!isPaused) {
       interval = setInterval(incrementCounter, 1000)
       pauseBtn.textContent = ' pause '
       minusBtn.disabled = false
@@ -49,26 +53,20 @@ document.addEventListener('DOMContentLoaded', () => {
       plusBtn.disabled = true
       heartBtn.disabled = true
     }
-  })
+  }
+  pauseBtn.addEventListener('click', pause)
 
-  heartBtn.addEventListener('click' , () => {
-    // "Like" an individual number of the counter. I should see the count of the number of "likes" associated with that number displayed.
-    //does likeObj have a key of [number]?
-    //yes increament likesObj[number]
+  heartBtn.addEventListener('click', () => {
+
     if (likeObj[number]) {
-      likeObj[number]++ 
-      // '2 has been liked 3 times
-      
-      let updateLi = document.querySelector('[data-num="'+number+'"]')
+      likeObj[number]++
+
+      let updateLi = document.querySelector('[data-num="' + number + '"]')
       updateLi.textContent = `${number} has been liked ${likeObj[number]} times`
       likesList.appendChild(updateLi)
     } else {
-      //no likesObj[number] = 1
       let li = document.createElement('li')
       li.setAttribute("data-num", number.toString())
-      // console.log('li is', li)
-
-      // console.log('num is', number)
       likeObj[number] = 1
       // '2 has been liked 1 time'
       li.textContent = `${number} has been liked 1 time`
@@ -79,9 +77,21 @@ document.addEventListener('DOMContentLoaded', () => {
   const restartBtn = document.createElement('button')
   restartBtn.id = 'restart'
   restartBtn.textContent = 'restart'
-  console.log('restart ', restartBtn)
   body.append(restartBtn)
+  // Click the "restart" button to restart the counter and re-enable the buttons.
+  function resetCounter() {
+    //clearInterval
+    counter.textContent = '0'
+    number = Number(counter.textContent)
+    isPaused = false
+    clearInterval(interval)
+    interval = setInterval(incrementCounter, 1000)
+    pauseBtn.textContent = ' pause '
+    minusBtn.disabled = false
+    plusBtn.disabled = false
+    heartBtn.disabled = false
+  }
+  restartBtn.addEventListener('click', resetCounter)
 })
 
-// Click the "restart" button to restart the counter and re-enable the buttons.
 // Leave comments on my gameplay, such as: "Wow, what a fun game this is."
